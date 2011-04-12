504bf75628ba4068b0de9117df2a49654fb69dd0
Chetan Sarva <chetan@pixelcop.net>
2011-04-11 23:37:16 -0400

    implemented window saving

--- tabsaver/sessions.js
+++ tabsaver/sessions.js
@@ -1,4 +1,42 @@
 
+function saveCurrentWindow(close) {
+
+    chrome.windows.getCurrent(function(win) {
+
+        var name = prompt("Enter a name or keyword for this window");
+        if (!name) {
+            name = "Saved window"
+        }
+
+        chrome.tabs.getAllInWindow(win.id, function(win_tabs) {
+
+            var tabs = saveTabs(win_tabs);
+            var session = { name: name, time: new Date().getTime(), tabs: tabs };
+
+            var windows = loadWindows();
+            windows.unshift(session);
+            localStorage.windows = JSON.stringify(windows);
+
+            if (close) {
+                chrome.windows.remove(win.id);
+            }
+        });
+
+    });
+    window.close();
+}
+
+function loadWindows() {
+    var windows;
+    if (localStorage.windows) {
+        windows = JSON.parse(localStorage.windows);
+    }
+    if (typeof(windows) != "object") {
+        windows = [];
+    }
+    return windows;
+}
+
 function loadSessions() {
     var sessions;
     if (localStorage.autosave_sessions) {
